1d8abf301bcf7b61dff22e5dd16ef953552e003f
diljar <diljarkurum@gmail.com>
2020-09-14 01:18:26 +0300

    barnch1 first commit

--- src/main/java/com/dk/demo1/Demo1Application.java
+++ src/main/java/com/dk/demo1/Demo1Application.java
@@ -1,13 +1,24 @@
 package com.dk.demo1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		log.info("Barnch 1");
 	}
 
 }
